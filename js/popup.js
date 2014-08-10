cee829a24d1fe795578ef517285e840843705069
Alek <agrinb@gmail.com>
2014-08-10 00:18:19 -0400

    added a URL fetcher from the tab

--- js/popup.js
+++ js/popup.js
@@ -1,18 +1,37 @@
 var Keywords = [];
 
+
 $(document).ready(function(){
+   $('form').on('submit',function(event){
+    event.preventDefault();
+   });
+
+   chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+    console.log(tabs[0].url);
+    console.log(tabs[0].url);
+    console.log(tabs[0].url);
+    });
+
+
+    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+         $('.tab_url').html(tabs[0].url);
+      });
+
+    var tab_url = $('.tab_url').find('p').text
+      console.log(tab_url)
+
   $('.log-twitter').on('click', function(event){
     console.log($('#keyword').val());
     if($('#keyword').val().length > 2 ){
-      Keywords.push($('#keyword').val())
+      Keywords.push($('#keyword').val());
+      $('#keyword').val("");
+      for(i = 0; i <= Keywords.length; i++ ){
+        var KeywordsOnPage = $.map(Keywords, function(keyword, index) {
+        return $('<p>'+keyword+'</p>')
+        })
+        $('.keyword-list').html(KeywordsOnPage);
+      };
     };
   });
-
-  while (Keywords > 0) {
-    var KeywordsOnPage = $.map(Keywords, function( keyword, index) {
-      return $('<li>keyword</li>')
-    })
-    $('.keyword-list').find('ul').html(KeywordsOnPage);
-  };
 });
 
