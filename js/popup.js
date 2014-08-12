59332122767fa48e18002d439895c2c14c67ffc4
Alek <agrinb@gmail.com>
2014-08-12 17:17:22 -0400

    moving to hosted app

--- js/popup.js
+++ js/popup.js
@@ -1,37 +1,60 @@
-var Keywords = [];
+
 
 
 $(document).ready(function(){
-   $('form').on('submit',function(event){
-    event.preventDefault();
-   });
 
-   chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-    console.log(tabs[0].url);
-    console.log(tabs[0].url);
-    console.log(tabs[0].url);
-    });
+  var tablink = ""
+  chrome.tabs.getSelected(null,function(tab) {
+    tablink = tab.url;
+    console.log(tablink)
+    $('.tab_url').html(tablink)
+  });
 
+  chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
+    console.log(token)
+  });
 
-    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-         $('.tab_url').html(tabs[0].url);
-      });
-
-    var tab_url = $('.tab_url').find('p').text
-      console.log(tab_url)
-
+  // $("#google_login_link").click(
+  //     function () {
+  //       console.log("Identity:", chrome.identity);
+  //       chrome.identity.getAuthToken({ 'interactive': false },function (token) {
+  //         alert("back token=" + token);
+  //         console.log("Identity:", chrome.identity);
+  //       });
+  //     });
+  var keywords = [];
   $('.log-twitter').on('click', function(event){
     console.log($('#keyword').val());
     if($('#keyword').val().length > 2 ){
-      Keywords.push($('#keyword').val());
+      keywords.push($('#keyword').val());
       $('#keyword').val("");
-      for(i = 0; i <= Keywords.length; i++ ){
-        var KeywordsOnPage = $.map(Keywords, function(keyword, index) {
+      for(i = 0; i <= keywords.length; i++ ){
+        var keywordsOnPage = $.map(keywords, function(keyword, index) {
         return $('<p>'+keyword+'</p>')
         })
-        $('.keyword-list').html(KeywordsOnPage);
+        $('.keyword-list').html(keywordsOnPage);
       };
     };
+  });
+
+
+
+  $( ".form-2" ).submit(function( event ) {
+  event.preventDefault();
+  console.log(keywords)
+  //   var form = $(this);
+  //   $.ajax('/companies', {
+  //     timeout: 3000,
+  //     type: 'POST'
+  //     // data: {"uid": API request result,
+  //     //         "keywords": keywords,
+  //     //         "url": tablink };
+  //     success: function(result){
+  //       form.remove();
+  //         $.('.api_response').html('<p>Job search saved.</p')
+  //       },
+  //       contentType: 'application/json'
+  //   });
   });
 });
 
