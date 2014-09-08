91a741adf261c35b8e66eb37f67de2a5ce4ebbdd
agrinb <agrinb@gmail.com>
2014-09-08 16:33:32 -0400

    cleaned up popup.js from comments

--- js/popup.js
+++ js/popup.js
@@ -1,14 +1,4 @@
 
-//$(document).ready(function(){
-
-  // $("#google_login_link").click(
-  //     function () {
-  //       console.log("Identity:", chrome.identity);
-  //       chrome.identity.getAuthToken({ 'interactive': false },function (token) {
-  //         alert("back token=" + token);
-  //         console.log("Identity:", chrome.identity);
-  //       });
-  //     });
 
   var tablink = null;
 
@@ -25,8 +15,6 @@
 
   var keywords = [];
   $('.log-twitter').on('click', function(event){
-    console.log($('#keyword').val());
-    console.log(user_uid);
     if($('#keyword').val().length > 2 ){
       keywords.push($('#keyword').val());
       $('#keyword').val("");
@@ -39,26 +27,7 @@
     };
   });
 
-  // $( ".form-2" ).submit(function( event ) {
-  // event.preventDefault();
-  // console.log(keywords);
-  // var interactive = true;
-  // getUserInfo(interactive);
-  // console.log(tablink);
-  // console.log(user_uid);
-  // console.log(user_email);
-  // console.log(keywords);
 
-    /*$.ajax({
-      type: "POST",
-      url: "http://localhost:3000/companies",
-      data: {"uid": user_uid, "keywords": keywords, "url": tablink, "user_email": user_email },
-      success: function(result){
-
-      },
-      dataType: "application/json"
-    });*/
-  // });
 
   $( ".form-2" ).submit(function( event ) {
     event.preventDefault();
@@ -66,25 +35,6 @@
     getUserInfo(interactive);
 
 
-    console.log("------");
-    console.log(getUserInfo(interactive));
-    /*user_uid = jQuery.parse(obj).uid;
-    user_email = jQuery.parse(obj).email;*/
-    console.log("form " + tablink);
-    console.log("form " + user_uid);
-    console.log("form " + user_email);
-    console.log("form " + keywords);
-
-    // $.ajax({
-    //   type: "POST",
-    //   url: "http://localhost:3000/companies",
-    //   data: {uid: user_uid, keywords: keywords, url: tablink, user_email: user_email },
-    //   success: function(result){
-    //     console.log("++++");
-    //     console.log(result);
-    //     },
-    //   dataType: "application/json"
-    // });
   });
-//});
+
 
