dec94616c619e8462e7c3af7bf6dda37feae18bb
Alek <agrinb@gmail.com>
2014-12-20 21:20:27 -0500

    Notifications are working, buttons added - not working yet

--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,8 @@
+$(document).ready(function(){
+   $('body').on('click', 'a', function(){
+     chrome.tabs.create({url: $(this).attr('href')});
+     return false;
+   });
 
 
   var tablink = null;
@@ -45,24 +50,52 @@
   var pusher = new Pusher('2089ef65bd0d820cb915');
   var channel = pusher.subscribe('1');
   channel.bind('my_event', function(data) {
-    processJobs(data);
+    var item_array = new Array();
+    for (var i = 0; i < 10; i++) {
+      item_array.push({ title: data[i]['title'], message: ""});
+    }
+      
+    var opt = {
+      type: "list",
+      title: "New Jobs Discovered",
+      message: "New Jobs Discovered",
+      iconUrl: "job_icon.png",
+      items: item_array
+    };
+
+    opt.buttons = [{title: "View Jobs"}, {title: "Not Right Now"}]
+  
+
+   // processJobs(data);
+    chrome.notifications.onButtonClicked.addListener(viewBtnClick);
+    chrome.notifications.onButtonClicked.addListener(closeBtnClick);
+    chrome.notifications.create("job-alert", opt, function(){});
   });
 
-  function processJobs(data){
-    var list = $("<ul></ul>");
-    for (var i = 0; i < data.length; i++) {
-      var title = data[i]['title'];
-      var url = data[i]['url'];
-      var li = "<li><a href="+url+">"+title+"</a></li>";
-      list = list.append(li);
-    }
-    buildJobResults(list);
-  };
+  function viewBtnClick(){};
+  function closeBtnClick(){};
 
-  function buildJobResults(list){
-    $('form').remove('.form-2');
-    $('.main').append(list);
-    $( "ul" ).addClass("job-list");
-    $('.job-list').find('li').addClass("job");
-  };
+  // function processJobs(data){
+  //   var list = $("<ul></ul>");
+  //   for (var i = 0; i < data.length; i++) {
+  //     var title = data[i]['title'];
+  //     var url = data[i]['url'];
+  //     var li = "<li><a href="+url+">"+title+"</a></li>";
+  //     list = list.append(li);
+  //   }
+  //   buildJobResults(list);
+  // };
+
+  // function buildJobResults(list){
+  //   $('form').remove('.form-2');
+  //   $('.main').append(list);
+  //   $( "ul" ).addClass("job-list");
+  //   $('.job-list').find('li').addClass("job");
+  // };
+
+
+/////////////////// chrome notification ////////////////////////////
+  
+});
+
 
